e6b4f0c361c625e9cbb62edb10f701f27e31f00d
Richard Liao <liao098@gmail.com>
2020-02-12 21:20:50 +0000

    added handleUserInput

--- client.js
+++ client.js
@@ -2,6 +2,7 @@ const net = require("net");
 /**
  * Establishes connection with the game server
  */
+
 const connect = function() {
   const conn = net.createConnection({
     host: "192.168.88.177",
@@ -11,14 +12,18 @@ const connect = function() {
   conn.setEncoding("utf8");
   conn.on("data", data => {
     console.log(data);
+    conn.write("Move: right");
   });
+
   conn.on("connect", () => {
     console.log("Successfully Connected!");
-    conn.write("Name:4G");
+    conn.write("Name: 4G");
+    conn.write("Move: up");
+    // setInterval(() => {
+    //   conn.write("Move: up");
+    // }, 500);
   });
   return conn;
 };
 
-module.exports = {
-  connect
-};
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,3 +1,48 @@
 const { connect } = require("./client");
 console.log("Connecting ...");
 connect();
+
+/**
+ * Setup User Interface
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", key => {
+    process.stdout.write(handleUserInput(key));
+  });
+  return stdin;
+};
+
+const handleUserInput = key => {
+  if (key === "\u0003") {
+    process.exit();
+  }
+  if (key === "w") {
+    return "Move: up";
+  }
+  if (key === "a") {
+    return "Move: left";
+  }
+  if (key === "s") {
+    return "Move: down";
+  }
+  if (key === "d") {
+    return "Move: right";
+  }
+  return key;
+};
+
+// stdin.on("data", key => {
+//   if (key === "\u0003") {
+//     process.exit();
+//   }
+//   if (key === "w") {
+//     stdin.write("Move: up");
+//   }
+// });
+setupInput();
